596809bfa2886145ff934f09728cd1b82e4d6267
Oliver <olichase91@gmail.com>
2023-04-09 17:14:17 -0700

    Solved Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+# def return_evens(num_list):
+#     evens = []
+#     for i in num_list:
+#         if(i % 2 == 0):
+#             evens.append(i)
+#     return evens
 
+# def make_exclamation(sentence_list):
+#     exclamations = []
+#     for i in sentence_list:
+#         exclmations.append(i + '!')
+#     return exclmations
+        
+        
+        
 def make_exclamation(sentence_list):
-    pass
+    excl = [(i + '!') for i in sentence_list]
+    return excl
+
+
+def return_evens(num_list):
+    return [i for i in num_list if i % 2 == 0]
